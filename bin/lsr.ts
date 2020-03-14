be3601b1902ebec34183cd922640a7eeb7572c00
Bryce Ghim <buttonfly@gmail.com>
2020-03-15 01:52:38 +0900

    update

---
+++ bin/lsr.ts
@@ -0,0 +1,13 @@
+'use strict'
+
+import ls from '../lib/ls'
+
+var args = process.argv.slice(2);
+console.log('args: ', args);
+
+ls({
+  path: args[0], 
+  callback: (path: string)=> {
+    console.log(`${args[0]}${path}`)
+  }
+})
