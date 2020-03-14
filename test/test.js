5618e10c5ffaa63f9ad4e9614bd0cd90f69f2d2c
Bryce Ghim <buttonfly@gmail.com>
2020-03-15 01:45:32 +0900

    update test

--- test/test.js
+++ test/test.js
@@ -8,10 +8,11 @@ var should = chai.should()  // Using Should style
 describe('All', function() {
   describe('ls', function() {
     it('should return -1 when the value is not present', function() {
+      let testPath = '.'
       ls({
-        path: '.', 
+        path: testPath, 
         callback: (path)=> {
-          console.log(path)
+          console.log(`${testPath}${path}`)
         }
       })
       assert.equal(1, 1)
